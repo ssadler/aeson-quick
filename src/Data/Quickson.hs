c4133982295e860ff347d244f958ee7afe4cfbe0
Scott Sadler <ssadler@mashi.org>
2014-08-23 13:56:27 +0200

    document and make cabal happy

--- src/Data/Quickson.hs
+++ src/Data/Quickson.hs
@@ -1,6 +1,18 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Data.Quickson where
+module Data.Quickson
+    (
+    -- * How to use this library
+    -- $use
+
+    -- * Syntax
+    -- $syntax
+      Quickson(..)
+    , quicksonParse
+    , quicksonExecute
+    , quickson
+    ) where
+
 
 import           Control.Monad
 import           Control.Applicative
@@ -44,9 +56,41 @@ quicksonExecute que = AT.parseEither (drill que >=> parseJSON)
     look v (k,False,qq) = v.:k >>= drill qq
 
 
--- | Perform a JSON extraction
+-- | Perform a JSON extraction, returning either an error description
+--   or a parsed data structure
 quickson :: FromJSON a => BS.ByteString -> BS.ByteString -> Either String a
 quickson structureSpec bs = do
     val <- eitherDecodeStrict bs
     query <- quicksonParse structureSpec
     quicksonExecute query val
+
+
+-- $use
+--
+-- Quickson exports a function `quickson` which enables you to perform quick
+-- extractions of JSON data using Aeson.
+--
+-- Aeson's type machinery allows decoding of complex data structures using
+-- just the 'decode' function, however, JSON object lookups cannot be encoded
+-- using the type system alone. Quickson helps by doing the lookups for you
+-- so that the type system can do the rest. For example, say you have a JSON
+-- document as such:
+--
+-- > { "name": "bob", "age": 25, "hobbies": [{"name": "Tennis"}] }
+--
+-- And you'd like to turn this into a `(String, Maybe Int, [String])` with
+-- minimal fuss:
+--
+-- > >>> type Hobbyist = (String, Maybe Int, [String])
+-- > >>> let eitherResult = quickson "{name,age?,hobbies:[{name}]}" jsonDoc :: Either String Hobbyist
+-- > Right ("bob",Just 25,["Tennis"])
+--
+-- So the structure specification is just to remove the objects so that the type
+-- system can do the rest.
+
+-- $syntax
+-- - Top level objects must be [] or {}
+-- - Lookup: {key}
+-- - Optional lookup: {key?} (yielding Maybe a)
+-- - List: []
+--
